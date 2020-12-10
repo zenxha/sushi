5612248a61afd0c9f7699a42b023e38ee72be31a
Christopher Rubin <jabba6789@gmail.com>
2020-12-10 21:44:19 +0000

    .

--- models/python.py
+++ models/python.py
@@ -4,16 +4,6 @@
 
 """ PYTHON ONLY """
 
-
-def python_ap():
-    group = "python"
-    route = ".pythonap"
-    title = "AP study big ideas outline"
-    timeline = "https://padlet.com/embed/k3bwade11ekevmum"
-    data = {"group": group, "route": route, "title": title, "timeline": timeline}
-    return data
-
-
 def python_hello():
     group = "python"
     route = ".pythonhello"
@@ -25,46 +15,3 @@ def python_hello():
     data = {"group": group, "route": route, "title": title, "timeline": timeline, "repo": repo,
             "repo_description": repo_description, "repl": repl}
     return data
-
-
-def python_flask():
-    group = "python"
-    route = ".pythonflask"
-    title = "Flask Web server introduction and ideas"
-    timeline = "https://padlet.com/embed/onshvns8f7vrrn2l"
-    repo = "https://gh-card.dev/repos/nighthawkcoders/flask-idea-portfolio.svg?link_target=_blank"
-    repo_description = "Ideas for building a Flask Web Server."
-    repl = "https://repl.it/@jmort1021/Python-Web-Portfolio-Series?lite=true"
-    data = {"group": group, "route": route, "title": title, "timeline": timeline, "repo": repo,
-            "repo_description": repo_description, "repl": repl}
-    return data
-
-
-def python_cbproj():
-    group = "python"
-    route = ".pythoncbproj"
-    title = "College Board project preparation focus"
-    timeline = "https://padlet.com/jmortensen7/cspcbproject"
-    repo = "https://gh-card.dev/repos/nighthawkcoders/flask-idea-homesite.svg?link_target=_blank"
-    repo_description = "Start process of building a project for College Board. Use my website or previous projects as " \
-                       "idea starters. "
-    data = {"group": group, "route": route, "title": title, "timeline": timeline, "repo": repo,
-            "repo_description": repo_description}
-    return data
-
-
-def python_study():
-    group = "python"
-    route = ".pythonstudy"
-    title = "Python study resources"
-    padlet = "https://padlet.com/jmortensen7/csp2021"
-    data = {"group": group, "route": route, "title": title, "padlet": padlet}
-    return data
-
-
-def python_projects():
-    return [python_hello(), python_flask(), python_cbproj(), python_ap(), python_study()]
-
-
-def python_details():
-    return {"title": 'CSP: Python', 'key': 'python'}
--- views.py
+++ views.py
@@ -1,31 +1,19 @@
 """Views in MVC has responsibility for establishing routes and redering HTML"""
 from flask import render_template, request, redirect, url_for
 from __init__ import app
-from models.java import java_ap, java_hello, java_mvc, java_event, java_study, java_details, java_projects
-from models.python import python_hello, python_ap, python_flask, python_cbproj, python_study, python_details, \
-    python_projects
+from models.python import python_hello
+import random, json
 
+backgrounds = ["https://cdn.discordapp.com/attachments/784178874303905792/784179064378359858/Gluten-free-sushi-rolls-header.png", "https://cdn.discordapp.com/attachments/784178874303905792/784179363100098560/wp6901896.png", "https://cdn.discordapp.com/attachments/784178874303905792/784179072531824650/onmyouji-anime-girls-anime-fan-art-brunette-hd-wallpaper-preview.png", "https://www.teahub.io/photos/full/193-1933361_laptop-aesthetic-wallpapers-anime.jpg"]
 
-backgrounds = ["https://cdn.discordapp.com/attachments/784178874303905792/784179064378359858/Gluten-free-sushi-rolls-header.png", "https://cdn.discordapp.com/attachments/784178874303905792/784179363100098560/wp6901896.png", "https://cdn.discordapp.com/attachments/784178874303905792/784179072531824650/onmyouji-anime-girls-anime-fan-art-brunette-hd-wallpaper-preview.png"]
-
+f = open('data.json')
+data = json.load(f)
 
 @app.route('/')
 def index():
-    
-    return render_template("homesite/home.html")
+  background = random.choice(backgrounds)
+  return render_template("homesite/home.html", background=background)
 
-
-@app.route('/landing/<selection>', methods=['GET', 'POST'])
-def landing(selection):
-    if request.method == 'POST':  # redirection to content page
-        form = request.form
-        page = form['page']
-        return redirect(url_for(page))
-    # based off of menu selection, content is selected for landing page
-    select_list = data[selection]
-    heading = select_list[TITLE]
-    projects = select_list[PROJECTS]
-    return render_template("homesite/landing.html", heading=heading, menus=menus, projects=projects)
 
 
 """our own project dstufsuf as"""
@@ -37,25 +25,17 @@ def project():
 @app.route('/base')
 def base():
   return render_template("homesite/base2.html")
+@app.route("/upload", methods=["GET", "POST"])
+def upload_image():
+  if request.method == "POST":
 
-@app.route('/java/mvc')
-def javamvc():
-    return render_template("homesite/project.html", menus=menus, data=java_mvc())
+      if request.files:
+          image = request.files["image"]
+          print(image)
+          return redirect(request.url)
 
+  return render_template("homesite/upload.html")
 
-@app.route('/java/ap')
-def javaap():
-    return render_template("homesite/project.html", menus=menus, data=java_ap())
-
-
-@app.route('/java/event')
-def javaevent():
-    return render_template("homesite/project.html", menus=menus, data=java_event())
-
-
-@app.route('/java/study')
-def javastudy():
-    return render_template("homesite/project.html", menus=menus, data=java_study())
 
 
 """Python Section"""
@@ -63,78 +43,4 @@ def javastudy():
 
 @app.route('/python/hello')
 def pythonhello():
-    return render_template("homesite/project.html", menus=menus, data=python_hello())
-
-
-@app.route('/python/flask')
-def pythonflask():
-    return render_template("homesite/project.html", menus=menus, data=python_flask())
-
-
-@app.route('/python/cbproj')
-def pythoncbproj():
-    return render_template("homesite/project.html", menus=menus, data=python_cbproj())
-
-
-@app.route('/python/ap')
-def pythonap():
-    return render_template("homesite/project.html", menus=menus, data=python_ap())
-
-
-@app.route('/python/study')
-def pythonstudy():
-    return render_template("homesite/project.html", menus=menus, data=python_study())
-
-
-"""Git Section"""
-
-
-@app.route('/git/concepts')
-def gitconcepts():
-    return render_template("homesite/project.html", menus=menus, data=git_concepts())
-
-
-@app.route('/git/replto')
-def gitreplto():
-    return render_template("homesite/project.html", menus=menus, data=git_replto())
-
-
-"""Pi Section"""
-
-
-@app.route('/pi/webserver')
-def piwebserver():
-    return render_template("homesite/project.html", menus=menus, data=pi_webserver())
-
-
-@app.route('/pi/portforward')
-def piportforward():
-    return render_template("homesite/project.html", menus=menus, data=pi_portforward())
-
-
-@app.route('/pi/vncsetup')
-def pivncsetup():
-    return render_template("homesite/project.html", menus=menus, data=pi_vncsetup())
-
-
-@app.route('/pi/realvnc')
-def pirealvnc():
-    return render_template("homesite/project.html", menus=menus, data=pi_realvnc())
-
-
-@app.route('/pi/ssh')
-def pissh():
-    return render_template("homesite/project.html", menus=menus, data=pi_ssh())
-
-
-"""PBL Section"""
-
-
-@app.route('/pbl/overview')
-def pbloverview():
-    return render_template("homesite/project.html", menus=menus, data=pbl_overview())
-
-
-@app.route('/pbl/scrum')
-def pblscrum():
-    return render_template("homesite/project.html", menus=menus, data=pbl_scrum())
+    return render_template("homesite/project.html", data=python_hello())
