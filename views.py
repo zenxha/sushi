0745f5b94898f364dead381bab15025f86b04d9a
zenxha <komays5042@gmail.com>
2021-01-07 13:47:46 -0800

    fix image paths

--- views.py
+++ views.py
@@ -9,7 +9,7 @@ from __init__ import app
 
 backgrounds = ["https://cdn.discordapp.com/attachments/784178874303905792/784179064378359858/Gluten-free-sushi-rolls-header.png", "https://cdn.discordapp.com/attachments/784178874303905792/784179363100098560/wp6901896.png", "https://www.teahub.io/photos/full/193-1933361_laptop-aesthetic-wallpapers-anime.jpg", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d8bea6d9-2ea4-457a-8ceb-b9acdf379496/d45k66i-f27635c2-c7ca-453b-9dad-dddc1d2c086f.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZDhiZWE2ZDktMmVhNC00NTdhLThjZWItYjlhY2RmMzc5NDk2XC9kNDVrNjZpLWYyNzYzNWMyLWM3Y2EtNDUzYi05ZGFkLWRkZGMxZDJjMDg2Zi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.3-YdpEq9vZOMQxfu2ESapQr9HsKmX2PJZo31rtpAcG0"]
 
-pathForImages='/images'
+pathForImages='images/'
 
 f = open('data.json')
 data = json.load(f)
