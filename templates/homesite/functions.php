bd41f77470f463b8b77f46f29764f316ed70acab
devamshri <devamshri@gmail.com>
2021-01-15 23:25:20 -0800

    database

--- templates/homesite/functions.php
+++ templates/homesite/functions.php
@@ -4,6 +4,6 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 if (!empty($username) || !empty($password){
 $dir = 'homesite/Users.db';
-$dataToStore = "INSERT INTO table_name (username, password) VALUES ('$username', '$password');"
+$dataToStore = "INSERT INTO users (username, password) VALUES ('$username', '$password');"
 file_put_contents($dir, $dataToStore);
 }
