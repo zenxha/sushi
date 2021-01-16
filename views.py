9597e549bbebf293642d3f33fee103ef04956319
Devam Shri <devamshri@gmail.com>
2021-01-15 22:35:54 -0800

    FORM ACTUALLY IS A COOL KID

--- views.py
+++ views.py
@@ -7,13 +7,13 @@ from flask import render_template, request, redirect, url_for, session, flash
 
 from __init__ import app
 
-backgrounds = [ "https://www.teahub.io/photos/full/193-1933361_laptop-aesthetic-wallpapers-anime.jpg"]
+backgrounds = ["https://cdn.discordapp.com/attachments/784178874303905792/784179064378359858/Gluten-free-sushi-rolls-header.png", "https://cdn.discordapp.com/attachments/784178874303905792/784179363100098560/wp6901896.png", "https://cdn.discordapp.com/attachments/784178874303905792/784179072531824650/onmyouji-anime-girls-anime-fan-art-brunette-hd-wallpaper-preview.png", "https://www.teahub.io/photos/full/193-1933361_laptop-aesthetic-wallpapers-anime.jpg"]
 
 pathForImages='./images/'
 
 f = open('data.json')
 data = json.load(f)
-DATABASE = 'templates/homesite/Users.db
+DATABASE = 'templates/homesite/Users.db'
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -24,7 +24,8 @@ def get_db():
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
-        db.close()'
+        db.close()
+
 @app.route('/')
 def index():
     #response = requests.get('https://nekos.life/api/v2/img/wallpaper')
@@ -76,6 +77,7 @@ def login():
         return render_template("homesite/login.html", background=background)
 @app.route('/loginv2', methods=["POST", 'GET'])
 def loginv2():
+    background = random.choice(backgrounds)
     if request.method == "POST":
         if request.files:
             image = request.files["image"]
@@ -84,7 +86,7 @@ def loginv2():
             print(name + ' uploaded a file with the name of ' + image.filename)
             return redirect(request.url)
 
-    return render_template("homesite/loginv2.html")
+    return render_template("homesite/loginv2.html", background=background)
 @app.route('/signup')
 @app.route('/user')
 def user():
