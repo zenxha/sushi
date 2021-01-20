78a13612713c45912f96936bcbcbd1c439187e8a
zenxha <komays5042@gmail.com>
2021-01-20 14:40:16 -0800

    OSError: [Errno 22] Invalid argument feelsbadman

--- views.py
+++ views.py
@@ -9,25 +9,30 @@ from __init__ import app
 
 backgrounds = ["https://www.teahub.io/photos/full/193-1933361_laptop-aesthetic-wallpapers-anime.jpg"]
 
-pathForImages='./images/'
+pathForImages = './images/'
 
 f = open('data.json')
 data = json.load(f)
 DATABASE = 'templates/homesite/Users.db'
+
+
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
+
 @app.route('/')
 def index():
-    #response = requests.get('https://nekos.life/api/v2/img/wallpaper')
+    # response = requests.get('https://nekos.life/api/v2/img/wallpaper')
     # background = response.json()['url']
     background = random.choice(backgrounds)
     return render_template("homesite/home.html", background=background)
@@ -35,21 +40,27 @@ def index():
 
 """our own project dstufsuf as"""
 
+
 @app.route('/project')
 def project():
     return render_template("homesite/project.html")
 
+
 @app.route('/base')
 def base():
-  return render_template("homesite/base2.html")
+    return render_template("homesite/base2.html")
+
 
 @app.route('/slideshow')
 def slideshow():
     return render_template("homesite/slideshow.html")
 
-app.config["IMAGE_UPLOADS"]="/images"
+
+app.config["IMAGE_UPLOADS"] = "/images"
 
 '''@app.route("/upload", methods=["GET", "POST"])'''
+
+
 def upload_image():
     if request.method == "POST":
 
@@ -63,6 +74,7 @@ def upload_image():
 
 
 "Login Section"
+
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
@@ -78,21 +90,26 @@ def login():
             flash("Already Logged In!")
             return redirect(url_for("user"))
         return render_template("homesite/login.html", background=background)
+
+
 @app.route('/upload', methods=["POST", 'GET'])
 def upload():
     background = random.choice(backgrounds)
     if request.method == "POST":
-        print("lol")
+        name = request.form["user_name"]
+        satisfactionlevel = request.form["satisfaction"]
+        print(request.form.get("satisfaction"))
+        print("\n")
         if request.files:
             image = request.files["image"]
             image.save(pathForImages + image.filename)
-            name = request.form["user_name"]
-            satisfaction = request.form["satisfaction"]
-            print(name + ' uploaded a file with the name of ' + image.filename + '\n' + "satisfaction level " + satisfaction)
+
+            print(name + ' uploaded a file with the name of ' + image.filename + '\n')
             return redirect(request.url)
 
     return render_template("homesite/loginv2.html", background=background)
-@app.route('/signup')
+
+
 @app.route('/user')
 def user():
     if "user" in session:
@@ -102,10 +119,11 @@ def user():
         flash("You are not logged in!")
         return redirect(url_for("login"))
 
+
 @app.route('/logout')
 def logout():
     if "user" in session:
         user = session["user"]
-        flash("You have been logged out!","warning")
+        flash("You have been logged out!", "warning")
     session.pop("user", None)
     return redirect(url_for("login"))
