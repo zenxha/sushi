a2796496d32d3b1eba2d96c793a0af77c009af2a
zenxha <komays5042@gmail.com>
2021-01-22 16:28:26 -0800

    change db name

--- views.py
+++ views.py
@@ -9,7 +9,7 @@ from db import db_init, db
 from model import Review
 app = Flask(__name__)
 # SQLAlchemy config. Read more: https://flask-sqlalchemy.palletsprojects.com/en/2.x/
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///img.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reviews.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db_init(app)
 
