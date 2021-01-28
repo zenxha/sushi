a0faa0b555806d0149426c457312bd6ffb48968c
devamshri <devamshri@gmail.com>
2021-01-28 14:04:40 -0800

    wsgi.py update

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,13 @@
 """Driver for Web server"""
-<<<<<<< HEAD
-=======
+#<<<<<<< HEAD
+#=======
 
->>>>>>> 5c797e696b60bc64ce3f06d7ea11ae061cd7a739
+#>>>>>>> 5c797e696b60bc64ce3f06d7ea11ae061cd7a739
 from views import app
 
 if __name__ == "__main__":
     # runs the application on the repl development server
-<<<<<<< HEAD
+#<<<<<<< HEAD
     app.run(debug=True,host='192.168.1.85',port=5000)
-=======
-    app.run(debug=False,host='192.168.1.182',port=5000)
->>>>>>> 5c797e696b60bc64ce3f06d7ea11ae061cd7a739
+#=======
+#>>>>>>> 5c797e696b60bc64ce3f06d7ea11ae061cd7a739
