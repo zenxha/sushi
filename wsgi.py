69b72231827fd80e067c776200210ab58f01710c
Allen Xu <allenyxu@users.noreply.github.com>
2021-02-01 14:25:01 -0800

    I am section to easteregg

--- wsgi.py
+++ wsgi.py
@@ -8,6 +8,6 @@ from views import app
 if __name__ == "__main__":
     # runs the application on the repl development server
 #<<<<<<< HEAD
-    app.run(debug=True,host='192.168.1.85',port=5000)
+    app.run(debug=True,host='127.0.0.1',port=5000)
 #=======
 #>>>>>>> 5c797e696b60bc64ce3f06d7ea11ae061cd7a739
