986915454ff02a9adf9dd25a281d2977b4b0c62f
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-03-05 15:47:40 -0800

    update model.py

--- templates/homesite/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-if (!empty($username) || !empty($password){
-$dir = 'homesite/Users.db';
-$dataToStore = "INSERT INTO users (username, password) VALUES ('$username', '$password');"
-file_put_contents($dir, $dataToStore);
-}
