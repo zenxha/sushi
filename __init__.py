37a68fd12bd9ad9e9929d0e59d54119b834ebc69
zenxha <komays5042@gmail.com>
2021-03-05 15:30:56 -0800

    track modifications

--- __init__.py
+++ __init__.py
@@ -11,6 +11,7 @@ app.secret_key = 'heyheyhey'
 app.permanent_session_lifetime = timedelta(hours=24)
 app.config['SECRET_KEY'] = 'heyheyhey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
