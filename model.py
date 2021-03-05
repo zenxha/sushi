c20f78eb2f0f408c97461f5a39b8422b24dbc547
zenxha <komays5042@gmail.com>
2021-03-05 15:19:52 -0800

    testing

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.Text, nullable=False)
     content = db.Column(db.Text, nullable=True)
-    img = db.Column(db.Text, unique=False, nullable=False)
+    img = db.Column(db.Text, unique=False, nullable=True)
     filename = db.Column(db.Text, nullable=False)
     satisfaction = db.Column(db.Text)
     mimetype = db.Column(db.Text, nullable=False)
